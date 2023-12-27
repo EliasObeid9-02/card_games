a1b6a5dc8a68fb6e6c91203f7c6dbfa7f0ca306c
mouhamadaloush <mouhamad.aloush06@gmail.com>
2023-12-27 16:26:57 +0300

    Room model added

---
+++ home/games.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class Game(ABC):
+    pass
+
+class Kemps(Game):
+    pass
+
+class Trix(Game):
+    pass
--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from .games import Game
 # Create your models here.
+
+class Player:
+    user = models.ForeignKey(User, editable=False)
+    nickname = models.CharField(max_length=200,default=user.username)
+
+
+
+class Room(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    game = models.ForeignKey(Game, editable=False)
+    player1 = models.ForeignKey(Player, editable=False)
+    player2 = models.ForeignKey(Player, editable=False)
+    player3 = models.ForeignKey(Player, editable=False)
+    player4 = models.ForeignKey(Player, editable=False)
+    is_done = models.BooleanField(default=False)
+    
+    
